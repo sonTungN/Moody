acb95d6ae4bd82a7beb0521a15dc78942309fd75
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-17 00:34:49 +0700

    feat: configure secret plugin

--- app/src/main/java/vn/edu/rmit/data/service/module/SupabaseModule.kt
+++ app/src/main/java/vn/edu/rmit/data/service/module/SupabaseModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.serializer.KotlinXSerializer
 import io.github.jan.supabase.storage.Storage
+import vn.edu.rmit.BuildConfig
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,8 +15,8 @@ object SupabaseModule {
     @Provides
     fun provideSupabase() =
         createSupabaseClient(
-            "https://yowjbglnodagjukzsbom.supabase.co",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlvd2piZ2xub2RhZ2p1a3pzYm9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMzkyMjcsImV4cCI6MjA1MjYxNTIyN30.mGz9kgS8nxW2L0XKOQ01IdXZr3llMYEfk0WsnNMwfe0"
+            BuildConfig.SUPABASE_ENDPOINT,
+            BuildConfig.SUPABASE_ANON_KEY
         ) {
             //Already the default serializer, but you can provide a custom Json instance (optional):
             defaultSerializer = KotlinXSerializer()
